30adfbddac11a60ea05aa4fc0593552d9233da76
arthurchvs@gmail.com <arthurchvs@gmail.com@33cf8d34-7881-45ee-2437-e99271344a79>
2012-04-12 12:31:37 +0000

    adicionada camera

--- trunk/jetpack/src/Load/ModelViewer.java
+++ trunk/jetpack/src/Load/ModelViewer.java
@@ -28,39 +28,41 @@
 
 package Load;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import javax.media.opengl.GL;
-import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLCapabilities;
-import javax.swing.JFrame;
+import com.sun.opengl.util.GLUT;
+import javax.media.opengl.*;
+import javax.media.opengl.glu.GLU;
+
 
 /**
- * Multiple JWavefront object viewer.
+ * JWavefront object viewer.
  *
  * @author Fernando V. Paulovich
  */
-public class TestMultipleModelViewer extends GLJPanelInteractive {
+public class ModelViewer implements GLEventListener{
+    public static int W = 800, H = 600;
+    public static float lx = 0, ly = 0, lz = 1, cx = 0, cy = 0, cz = 0, angle = 90, yangle = 0;
+    
+    public ModelViewer(GLCapabilities glcaps, JWavefrontModel model) {
 
-    public TestMultipleModelViewer(GLCapabilities glcaps, ArrayList<JWavefrontModel> models) {
-        super(glcaps);
-        this.models = models;
+        this.model = model;
     }
 
     @Override
     public void init(GLAutoDrawable glad) {
         //OpenGl Parameters
         GL gl = glad.getGL();
+        GLU glu = new GLU();
         gl.glEnable(GL.GL_LIGHTING);
         gl.glEnable(GL.GL_LIGHT0);
         gl.glEnable(GL.GL_DEPTH_TEST);
         gl.glShadeModel(GL.GL_SMOOTH);
+        gl.glMatrixMode(GL.GL_PROJECTION); // carrega a matriz de projeção
+        gl.glLoadIdentity(); // le a matriz identidade
+        gl.glViewport(-200, -200, 400, 400);
+        glu.gluPerspective(60, 1.0f, 0.1f, 200); 
 
-        //compiling the objects
-        for (JWavefrontModel model : models) {
-            model.compile(glad, JWavefrontModel.WF_MATERIAL | JWavefrontModel.WF_TEXTURE | JWavefrontModel.WF_SMOOTH);
-        }
+        //compiling the object
+        model.compile(glad, JWavefrontModel.WF_MATERIAL | JWavefrontModel.WF_TEXTURE | JWavefrontModel.WF_SMOOTH);
 
         lighting(glad);
     }
@@ -68,17 +70,21 @@ public class TestMultipleModelViewer extends GLJPanelInteractive {
     @Override
     public void display(GLAutoDrawable glad) {
         GL gl = glad.getGL();
-
+        GLU glu = new GLU();
+        
         gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f); //backgroung color
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-
+        
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity(); //carrega matrix identidade
+        System.out.println("angle: " + angle + " cx: " + cx + " cy: " + cy + 
+                           " cz: " + cz + " lx: " + lx + " ly: " + ly + " lz: " + lz);
+        glu.gluLookAt(	cx, cy, cz,
+			cx+lx, cy+ly, cz+lz,
+			0.0f, 1.0f,  0.0f);
+        
         //draw the object
-        for (int i = 0; i < models.size(); i++) {
-            float trans = 2 * (i - (models.size() / 2));
-            gl.glTranslatef(trans, 0, 0);
-            models.get(i).draw(glad);
-            gl.glTranslatef(-trans, 0, 0);
-        }
+        model.draw(glad);
 
         gl.glFlush(); //execute all commands
     }
@@ -97,9 +103,7 @@ public class TestMultipleModelViewer extends GLJPanelInteractive {
         float[] luzAmbiente = {0.3f, 0.3f, 0.3f, 1.0f};
         float[] luzDifusa = new float[]{0.75f, 0.75f, 0.75f, 1.0f};
         float[] luzEspecular = new float[]{0.7f, 0.7f, 0.7f, 1.0f};
-        float[] posicaoLuz = new float[]{GLJPanelInteractive.WINDOW_SIZE * 5.0f,
-            GLJPanelInteractive.WINDOW_SIZE * 5.0f, GLJPanelInteractive.WINDOW_SIZE * 5.0f, 1.0f
-        };
+        float[] posicaoLuz = new float[]{5 * 5.0f, 5 * 5.0f, 5 * 5.0f, 1.0f};
 
         // Define os parametros da luz de numero 0
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, luzAmbiente, 0);
@@ -108,36 +112,7 @@ public class TestMultipleModelViewer extends GLJPanelInteractive {
         gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, posicaoLuz, 0);
     }
 
-    public static void main(String args[]) throws IOException {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-
-        ArrayList<JWavefrontModel> models = new ArrayList<JWavefrontModel>();
-        models.add(new JWavefrontModel(new File("./data/chapel/chapel.obj")));
 
 
-        for (JWavefrontModel model : models) {
-            model.unitize();
-            model.facetNormals();
-            model.vertexNormals(90);
-            model.dump(false);
-        }
-
-        //creting the projection panel
-        GLCapabilities glcaps = new GLCapabilities();
-        glcaps.setAccumBlueBits(16);
-        glcaps.setAccumGreenBits(16);
-        glcaps.setAccumRedBits(16);
-        glcaps.setDoubleBuffered(true);
-        glcaps.setHardwareAccelerated(true);
-
-        TestMultipleModelViewer viewer = new TestMultipleModelViewer(glcaps, models);
-        viewer.setOpaque(true);
-
-        frame.getContentPane().add(viewer);
-        frame.setVisible(true);
-    }
-
-    private ArrayList<JWavefrontModel> models;
+    private JWavefrontModel model;
 }
