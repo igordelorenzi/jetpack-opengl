e342c20c9776ca66487e6e0a3b60aea2ad7d0532
brunojadami@gmail.com <brunojadami@gmail.com@33cf8d34-7881-45ee-2437-e99271344a79>
2012-04-28 11:59:46 +0000

    esqueci de adicionar estes arquivos

---
+++ trunk/jetpack/src/Main/Engine.java
@@ -0,0 +1,111 @@
+
+
+package Main;
+
+import Load.JWavefrontModel;
+import Object.WorldObject;
+import com.sun.opengl.util.GLUT;
+import java.util.Iterator;
+import java.util.LinkedList;
+import javax.media.opengl.*;
+import javax.media.opengl.glu.GLU;
+
+
+public class Engine implements GLEventListener
+{
+    private LinkedList<WorldObject> objectsList = new LinkedList<WorldObject>();
+    public static int W = 800, H = 600;
+    public static float lx = 0, ly = 0, lz = 1, cx = 0, cy = 0, cz = 0, angle = 90, yangle = 0;
+    
+    public Engine() 
+    {
+        
+    }
+    
+    public void addObject(WorldObject object)
+    {
+        objectsList.add(object);
+    }
+
+    @Override
+    public void init(GLAutoDrawable glad) {
+        //OpenGl Parameters
+        GL gl = glad.getGL();
+        GLU glu = new GLU();
+        gl.glEnable(GL.GL_LIGHTING);
+        gl.glEnable(GL.GL_LIGHT0);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glShadeModel(GL.GL_SMOOTH);
+        gl.glMatrixMode(GL.GL_PROJECTION); // carrega a matriz de projeção
+        gl.glLoadIdentity(); // le a matriz identidade
+        //gl.glViewport(-1, -1, 2, 2);
+        glu.gluPerspective(60, 1.0f, 0.1f, 200); 
+
+        Iterator<WorldObject> it = objectsList.iterator();
+        while (it.hasNext())
+        {
+            it.next().getModel().compile(glad, JWavefrontModel.WF_MATERIAL | JWavefrontModel.WF_TEXTURE | JWavefrontModel.WF_SMOOTH);
+        }
+        lighting(glad);
+    }
+    
+    private void updateGame()
+    {
+        Iterator<WorldObject> it = objectsList.iterator();
+        while (it.hasNext())
+        {
+            it.next().update();
+        }
+    }
+
+    @Override
+    public void display(GLAutoDrawable glad) 
+    {
+        updateGame();
+        
+        GL gl = glad.getGL();
+        GLU glu = new GLU();
+        
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f); //backgroung color
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity(); //carrega matrix identidade
+        //System.out.println("angle: " + angle + " cx: " + cx + " cy: " + cy + 
+                          // " cz: " + cz + " lx: " + lx + " ly: " + ly + " lz: " + lz);
+        glu.gluLookAt(	cx, cy, cz,
+			cx+lx, cy+ly, cz+lz,
+			0.0f, 1.0f,  0.0f);
+        
+        Iterator<WorldObject> it = objectsList.iterator();
+        while (it.hasNext())
+        {
+            it.next().draw(glad);
+        }
+
+        gl.glFlush(); //execute all commands
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glad, int x, int y, int w, int h) {
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable glad, boolean bln, boolean bln1) {
+    }
+
+    private void lighting(GLAutoDrawable glad) {
+        GL gl = glad.getGL();
+
+        float[] luzAmbiente = {0.3f, 0.3f, 0.3f, 1.0f};
+        float[] luzDifusa = new float[]{0.75f, 0.75f, 0.75f, 1.0f};
+        float[] luzEspecular = new float[]{0.7f, 0.7f, 0.7f, 1.0f};
+        float[] posicaoLuz = new float[]{5 * 5.0f, 5 * 5.0f, 5 * 5.0f, 1.0f};
+
+        // Define os parametros da luz de numero 0
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, luzAmbiente, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, luzDifusa, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, luzEspecular, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, posicaoLuz, 0);
+    }
+}
---
+++ trunk/jetpack/src/Main/GameFrame.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import javax.swing.JFrame;
+import Input.MyKeyListener;
+import Input.MyMouseMotionListener;
+import Load.JWavefrontModel;
+import Object.Chapel;
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author bruno
+ */
+public class GameFrame extends JFrame
+{
+    private GraphicsDevice device;
+    private Engine viewer;
+    private GLCanvas canvas;
+    
+    public GameFrame()
+    {
+        
+    }
+    
+    public void start()
+    {
+        setVisible(true);
+        Animator animator = new FPSAnimator(canvas, 30);
+        animator.start();
+    }
+    
+    public void load() throws IOException
+    {
+        device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+        //creting the projection panel
+        GLCapabilities glcaps = new GLCapabilities();
+        
+        glcaps.setAccumBlueBits(16);
+        glcaps.setAccumGreenBits(16);
+        glcaps.setAccumRedBits(16);
+        glcaps.setDoubleBuffered(true);
+        glcaps.setHardwareAccelerated(true);
+        canvas = new GLCanvas(glcaps);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(Engine.W, Engine.H);
+        setLocationRelativeTo(null);
+        //frame.setUndecorated(true);
+        setResizable(false);
+        setCursor(getToolkit().createCustomCursor(
+                new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),
+                "null"));
+        if (device.isFullScreenSupported()) { // Go for full-screen mode
+            setIgnoreRepaint(true);     // Ignore OS re-paint request
+            //device.setFullScreenWindow(frame);
+        }
+        
+        viewer = new Engine();
+        canvas.addGLEventListener(viewer);
+        canvas.addMouseMotionListener(new MyMouseMotionListener());
+        addKeyListener(new MyKeyListener());
+        getContentPane().add(canvas);
+        
+        loadObjects();
+    }
+    
+    private void loadObjects() throws IOException
+    {
+        //model.dump(true);
+        viewer.addObject(new Chapel());
+    }
+}
